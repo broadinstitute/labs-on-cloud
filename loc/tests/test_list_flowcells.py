2d682a76e160a7aa71ae8edb5ddd5c0d70ffc0ec
jgould <jgould@broadinstitute.org>
2018-06-06 05:50:35 -0400

    updated return value

--- loc/tests/test_list_flowcells.py
+++ loc/tests/test_list_flowcells.py
@@ -14,7 +14,7 @@ class TestListFlowCells(unittest.TestCase):
         import os
         results = loc.filter_flow_cells_by_run_date(sequencing_dirs=['seq_dir'], days_old=6,
                                                     now=datetime(year=2018, month=5, day=21))
-        self.assertEqual('170622_M01581_1108_000000000-B949Y', os.path.basename(results[0]))
+        self.assertEqual('170622_M01581_1108_000000000-B949Y', os.path.basename(results[0]['path']))
         results = loc.filter_flow_cells_by_run_date(sequencing_dirs=['seq_dir'], days_old=6,
                                                     now=datetime(year=2019, month=5, day=21))
         self.assertEqual(len(results), 3)
